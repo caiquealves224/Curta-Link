25370ae60bcc6e322bab246d0beb1fd551bbbc57
caique <caiquealves224@gmail.com>
2025-07-08 22:57:33 -0300

    feat: add hot reload

--- src/controller.js
+++ src/controller.js
@@ -1,19 +1,20 @@
 export const encurtar = (req, res) => {
-  const { url } = req.body;
+  // const { url } = req.body;
   // Logic to shorten the URL
   // For example, you could generate a random string and save it in a database
-  const shortUrl = generateShortUrl(url);
-  
-  res.status(201).json({ shortUrl });
+  // const shortUrl = generateShortUrl(url);
+  res.status(201).json({ });
 }
 
 export const redirecionar = (req, res) => {
   const { shortUrl } = req.params;
   // Logic to find the original URL from the short URL
-  const originalUrl = findOriginalUrl(shortUrl);
-  if (originalUrl) {
-    res.redirect(originalUrl);
-  } else {
-    res.status(404).json({ mensagem: 'URL não encontrada.' });
-  }
+  // const originalUrl = findOriginalUrl(shortUrl);
+  // if (originalUrl) {
+  //   res.redirect(originalUrl);
+  // } else {
+  //   res.status(404).json({ mensagem: 'URL não encontrada.' });
+  // }
+
+  res.status(404).json({ mensagem: 'URL não encontrada.' });
 }

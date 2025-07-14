a5d62ad1993a117d0783e2d1e38a3098dd3dc86a
caique <caiquealves224@gmail.com>
2025-07-14 20:00:04 -0300

    fix: change imports

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { encurtar, redirecionar, listarUrls } from './controller.js';
+import { encurtar, redirecionar, listarUrls } from './controllers/controller.js';
 
 const router = Router();
 

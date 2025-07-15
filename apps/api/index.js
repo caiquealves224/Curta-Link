7dc4b146be408c200629ecfcd67ed71c9c1b5205
caique <caiquealves224@gmail.com>
2025-07-15 20:19:37 -0300

    feat: allow cors, create inputs

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import routes from './src/routes.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(routes);
 
